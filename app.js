c063c543602d1fe8c005d4461589ecebfafbcebe
AnPham79 <anpnb79@gmail.com>
2024-08-02 00:26:15 +0700

    monster slayer vuejs game

--- app.js
+++ app.js
@@ -1,63 +1,88 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
-function getRandomValue(max, min)
-{
-    return Math.floor(Math.random() * (max - min)) + min
+function getRandomValue(max, min) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const app = Vue.createApp({
+const app = createApp({
     data() {
         return {
-            playerHealth : 100,
-            monsterHealth : 100,
-            currentRound: 0
-        }
+            playerHealth: 100,
+            monsterHealth: 100,
+            currentRound: 0,
+            winner: null,
+            logMessages: []
+        };
     },
-    computed : {
+    computed: {
         monsterBarStyle() {
-            return {width: this.monsterHealth + '%' }
+            return { width: this.monsterHealth + '%' };
         },
         playerBarStyle() {
-            return { width: this.playerHealth + '%' }
+            return { width: this.playerHealth + '%' };
         },
         mayPlayerSpecialAttack() {
             return this.currentRound % 3 !== 0;
         },
-        mayPlayerHeal()
-        {
+        mayPlayerHeal() {
             return this.currentRound % 2 !== 0;
         }
     },
-    methods : {
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'player';
+            }
+        }
+    },
+    methods: {
         attackMonster() {
-            const attackValues = getRandomValue(12, 5);
-            this.monsterHealth = this.monsterHealth - attackValues;
-            this.currentRound++
-            this.attackPlayer()
+            const attackValue = getRandomValue(12, 5);
+            this.monsterHealth = Math.max(this.monsterHealth - attackValue, 0);
+            this.currentRound++;
+            this.addLogMessage('player', 'attack', attackValue);
+            this.attackPlayer();
         },
         attackPlayer() {
-            const attackMonsterValues = getRandomValue(12, 8)
-            this.playerHealth -= attackMonsterValues
+            const attackValue = getRandomValue(15, 8);
+            this.playerHealth = Math.max(this.playerHealth - attackValue, 0);
+            this.addLogMessage('monster', 'attack', attackValue);
         },
         specialAttackMonster() {
-            this.currentRound++
-            const specialAttackMonster = getRandomValue(25, 10)
-            this.monsterHealth -= specialAttackMonster
-        },
-        heal()
-        {
-            const healValue = getRandomValue(10, 5);
-            
-            if(this.playerHealth + healValue > 100) {
-                this.playerHealth = 100
-            } else {
-                this.playerHealth += healValue;
-            }
+            const attackValue = getRandomValue(25, 10);
+            this.monsterHealth = Math.max(this.monsterHealth - attackValue, 0);
+            this.currentRound++;
+            this.addLogMessage('player', 'special-attack', attackValue);
             this.attackPlayer();
-
-            this.currentRound++
+        },
+        heal() {
+            const healValue = getRandomValue(20, 8);
+            this.playerHealth = Math.min(this.playerHealth + healValue, 100);
+            this.currentRound++;
+            this.addLogMessage('player', 'heal', healValue);
+            this.attackPlayer();
+        },
+        surrender() {
+            this.winner = 'monster';
+            this.addLogMessage('player', 'surrender', 0);
+        },
+        addLogMessage(who, what, value) {
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            });
         }
     }
-})
+});
 
-app.mount('#game')
+app.mount('#game');
